63831b76241edef347f2679c9e34a055c38c9a6a
Amer Ibric <ameribric@gmail.com>
2022-05-21 20:31:05 -0400

    commit tree bc86ddb8fe1371fda5a93726e1fb4a61baec5150

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,35 @@
 require 'pry'
 
 class String
+  def report_on_self 
+    "Self is #{self}"
+  end
 
   def sentence?
-    
+    if self.includes?('.') 
+      true
+    else false
+    end
   end
 
   def question?
+    if self.end_with?('?')
+      true
+    else false
+    end
 
   end
 
   def exclamation?
-
+    if self.end_with?(!)
+      true
+    else false
+    end
   end
 
   def count_sentences
-
+    if self.split.count = 3
+      count = 3
+    else nil
   end
 end
